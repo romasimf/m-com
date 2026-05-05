94d52b928ec3f4535b58a29aef7e7c0dd8f4e958
Roma <simf_roma@mail.ru>
2026-05-05 18:54:50 +0300

    Добавлена возможность добавлять/удалять фото во время публикации/редактировании поста.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [ :show, :edit, :update, :destroy ]
 
   def new
     @post = current_user.posts.build
@@ -23,16 +23,52 @@ class PostsController < ApplicationController
   end
 
   def update
-    new_images = params.dig(:post, :images)&.reject(&:blank?)
+    removed_ids = params[:post][:removed_image_ids].to_s.split(",").reject(&:blank?)
+    new_images  = Array(params.dig(:post, :images)).reject(&:blank?)
+    new_body    = params[:post][:body].to_s.strip
 
-    if @post.update(body: params[:post][:body])
-      if new_images.present?
-        @post.images.purge
-        @post.images.attach(new_images)
+    current_images = @post.images.attachments.to_a
+    remaining_images_count = current_images.count - removed_ids.count
+    remaining_images_count = 0 if remaining_images_count.negative?
+
+    total_images = remaining_images_count + new_images.count
+
+    if total_images > 5
+      @post.assign_attributes(body: params[:post][:body])
+      @removed_image_ids = removed_ids
+      @post.errors.add(:images, "Можно загрузить не больше 5 фото")
+      render :edit, status: :unprocessable_entity
+      return
+    end
+
+    if new_body.blank? && total_images.zero?
+      @post.assign_attributes(body: params[:post][:body])
+      @removed_image_ids = removed_ids
+      @post.errors.add(:base, "Пост не может быть пустым")
+      render :edit, status: :unprocessable_entity
+      return
+    end
+
+    newly_attached = []
+
+    if new_images.present?
+      before_ids = @post.images.attachments.pluck(:id)
+      @post.images.attach(new_images)
+      after_attachments = @post.images.attachments.reload
+      newly_attached = after_attachments.reject { |attachment| before_ids.include?(attachment.id) }
+    end
+
+    @post.assign_attributes(body: params[:post][:body])
+
+    if @post.save
+      current_images.each do |attachment|
+        attachment.purge if removed_ids.include?(attachment.id.to_s)
       end
 
       redirect_to profile_path, notice: "Пост обновлён"
     else
+      newly_attached.each(&:purge)
+      @removed_image_ids = removed_ids
       render :edit, status: :unprocessable_entity
     end
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,23 @@
 class Post < ApplicationRecord
   belongs_to :user
+
   has_many_attached :images
 
-  validates :body, length: { maximum: 1000 }
+  validates :body, length: { maximum: 500 }
   validate :body_or_images_present
+  validate :images_count_within_limit
 
   private
 
   def body_or_images_present
     if body.blank? && !images.attached?
       errors.add(:base, "Пост не может быть пустым")
+    end
+  end
+
+  def images_count_within_limit
+    if images.count > 5
+      errors.add(:images, "можно загрузить не больше 5 фото")
     end
   end
 end
